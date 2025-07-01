51004b342796f0adee95a27dacd0c20f8b07db04
Kenrick-MH <kenrickmarcellhaditio@gmail.com>
2025-07-01 11:40:53 -0700

    Project structure for a software emulator, a bit of work on ROM

---
+++ software_emu/include/cart.h
@@ -0,0 +1,48 @@
+
+#include <common.h>
+
+
+/**
+ *  Based on the documentation in 
+ *  https://gbdev.io/pandocs/The_Cartridge_Header.html
+ */
+
+ /**
+  *  Struct defining the ROM header metadata upon loading.
+  */
+typedef struct cart 
+{
+    u8 nintendo_logo[0x30];
+    char title[0x10];
+    char manu_code[4];
+    u32 rom_size;
+    u32 ram_size;
+    u8 destination_code;
+    u8 cart_type;
+    u8 old_lisencee_code;
+    u8 new_lisencee_code;
+    u8 version;
+    u8 header_checksum;
+    u16 global_checksum;
+    u8 cgb_flag :1;
+    u8 sgb_flag :1;
+    
+} cart_meta_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ software_emu/include/common.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+// uint_t aliases
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+// int_t aliases
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+/*
+    Common macro defs here
+*/
+
+
+
+
+
