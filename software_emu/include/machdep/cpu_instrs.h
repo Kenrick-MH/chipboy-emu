9a40d98c48597547a6c5b183641b5fe63115d91d
Kenrick-MH <kenrickmarcellhaditio@gmail.com>
2025-07-12 18:52:01 -0700

    made all function declarations

--- software_emu/include/machdep/cpu_instrs.h
+++ software_emu/include/machdep/cpu_instrs.h
@@ -1,9 +1,9 @@
-
 #include <cpu.h>
 
 /**
  *  MACROS FOR INSTRUCTIONS
  */
+typedef void (*INSTR_FUNC)(cpu_context_t *);
 
 /** 
  *  Instructions for the CPU. 
@@ -17,152 +17,66 @@
  *  All functions takes (cpu_context *, uint8_t opcode) as an argument.
  */
 void instr_nop(cpu_context_t *context);
-
-/* ALU Operations */
-
-/*  */
-void instr_alu_op_reg(cpu_context_t *context, uint8_t reg_num,
-                        uint8_t alu_opcode);
-
-/* Adds 8-bit immediate to the accumulator register */
-void instr_alu_op_imm(cpu_context_t *context, uint8_t imm8, uint8_t alu_opcode);
-void instr_add_hl_r16(cpu_context_t *context, uint16_t src_reg);
-
-/* Increment 8-bit register by 8 */
-void instr_incr8(cpu_context_t *context, uint8_t reg8_num);
-
-/* Decrement 8-bit register by 8 */
-void instr_decr8(cpu_context_t *context, uint8_t reg8_num);
-
-/* Increment 16-bit register */
-void instr_incr16(cpu_context_t *context, uint8_t reg16_num);
-
-/* Decrement 16-bit register */
-void instr_decr16(cpu_context_t *context, uint8_t reg16_num);
-
-
-/**
- * =============================
- *  LOAD/STORE/MOV INSTRUCTIONS
- * =============================
- */
-
-/* Moves value from source register src_num to dest_num */
-void instr_mov_r8(cpu_context_t *context, uint8_t src_num, uint8_t dest_num);
-
-/* Moves immediate value into 8-bit register */
-void instr_mov_r8_imm8(cpu_context_t *context, uint8_t dest_num, uint8_t imm8);
-
-/* Moves 16-bit immediate values to 16-bit register */
-void instr_mov_r16_imm16(cpu_context_t *context);
-
-/* 
-    Load / Store instructions (these actually go to memory).
-    Here, HL is the address argument.
-*/
-void instr_ld_r8_hl(cpu_context_t *context);
-void instr_str_hl_r8(cpu_context_t *context);
-void instr_str_hl_imm8(cpu_context_t *context);
-
-/* 
-    16-bit load/stores. 
-    These take a 16-bit register/immediate as the address
-    argument, 
-    and loads/stores it to register A.
-*/
-void instr_ld_a_r16(cpu_context_t *context);
-void instr_ld_a_imm16(cpu_context_t *context);
-void instr_str_r16_a(cpu_context_t *context);
-void instr_str_imm16_a(cpu_context_t *context);
-
-/* 
-    Store to a location 0xFF00 + 8-bit value (ldh) instructions.
-    Data going to CPU is denoted as a ldh (load-half) instruction.
-    Data going out from CPU is denoted as sth (store-half) instruction.
-*/
-void instr_sth_imm8_a(cpu_context_t *context);
-void instr_sth_c_a(cpu_context_t *context);
-void instr_ldh_c_a(cpu_context_t *context);
-void instr_ldh_imm8_a(cpu_context_t *context);
-
-/*
-    Store and increment/decrement (corresponds to the instructions)
-    ld_(incr/decr) -> ld A [hl(i/d)] 
-    str_(incr/decr) -> ld [hl(i/d)] A
-*/
-void instr_ld_incr(cpu_context_t *context);
-void instr_ld_decr(cpu_context_t *context);
-void instr_str_incr(cpu_context_t *context);
-void instr_str_decr(cpu_context_t *context);
-
-
-/**
- * =============================
- *       STACK INSTRUCTIONS
- * =============================
- */
-
-/* Push/Pop */
-void instr_push16(cpu_context_t *context, uint8_t r16stk);
-void instr_pop16(cpu_context_t *context, uint8_t r16stk);
-
-/* Jumps */
-void instr_jp_hl(cpu_context_t *context);
-void instr_jp_cc(cpu_context_t *context, addr_t address, uint8_t condition);
-void instr_jr_cc(cpu_context_t *context, int8_t addr_offset, uint8_t condition);
-
-/* 
-    Has 2 different versions, since the always variant and not taken variant have
-    two differing clock cycles.
-*/
-void instr_ret(cpu_context_t *context);
-void instr_ret_cc(cpu_context_t *context, uint8_t condition);
-void instr_reti(cpu_context_t *context);
-
-void instr_call_cc(cpu_context_t *context, addr_t label, uint8_t condition);
-
-/* Bit Operations */
-void instr_bit_r8(cpu_context_t *context, uint8_t regcode, uint8_t bit_select);
-void instr_res_r8(cpu_context_t *context, uint8_t regcode, uint8_t bit_select);
-void instr_set_r8(cpu_context_t *context, uint8_t regcode, uint8_t bit_select);
-
-
-/*
-    ===============
-        
-    =============== 
-*/
+void instr_ld_r16_imm16(cpu_context_t *context);
+void instr_ld_r16mem_a(cpu_context_t *context);
+void instr_ld_a_r16mem(cpu_context_t *context);
+void instr_imm16mem_sp(cpu_context_t *context);
+void instr_inc_r16(cpu_context_t *context);
+void instr_dec_r16(cpu_context_t *context);
+void instr_add_hl_r16(cpu_context_t *context);
+void instr_inc_r8(cpu_context_t *context);
+void instr_dec_r8(cpu_context_t *context);
+void instr_ld_r8_imm8(cpu_context_t *context);
+void instr_rlca(cpu_context_t *context);
+void instr_rrca(cpu_context_t *context);
+void instr_rla(cpu_context_t *context);
+void instr_rra(cpu_context_t *context);
+void instr_daa(cpu_context_t *context);
 void instr_cpl(cpu_context_t *context);
-
-
-/**
- * =============================
- *       CARRY INSTRUCTIONS
- * =============================
- */
-/* Complement carry flag */
-void instr_ccf(cpu_context_t *context);
-
-/* Set carry flag */
 void instr_scf(cpu_context_t *context);
-
+void instr_ccf(cpu_context_t *context);
+void instr_jr_imm8(cpu_context_t *context);
+void instr_jr_cond_imm8(cpu_context_t *context);
+void instr_stop(cpu_context_t *context);
+void instr_ld_r8_r8(cpu_context_t *context);
+void instr_halt(cpu_context_t *context);
+void instr_alu_op_r8(cpu_context_t *context);
+void instr_alu_op_imm8(cpu_context_t *context);
+void instr_ret_cond(cpu_context_t *context);
+void instr_ret(cpu_context_t *context);
+void instr_reti(cpu_context_t *context);
+void instr_jp_cond(cpu_context_t *context);
+void instr_jp_imm16(cpu_context_t *context);
+void instr_jp_hl(cpu_context_t *context);
+void instr_call_cond_imm16(cpu_context_t *context);
+void instr_call_imm16(cpu_context_t *context);
+void instr_rst_tgt3(cpu_context_t *context);
+void instr_pop_r16stk(cpu_context_t *context);
+void instr_push_r16stk(cpu_context_t *context);
+void instr_cb_prefix(cpu_context_t *context);
+void instr_ldh_cmem_a(cpu_context_t *context);
+void instr_ldh_imm8mem_a(cpu_context_t *context);
+void instr_ld_imm16mem_a(cpu_context_t *context);
+void instr_ldh_a_cmem(cpu_context_t *context);
+void instr_ld_a_imm16mem(cpu_context_t *context);
+void instr_add_sp_imm8(cpu_context_t *context);
+void instr_ld_hl_sp_imm8(cpu_context_t *context);
+void instr_ld_sp_hl(cpu_context_t *context);
+void instr_di(cpu_context_t *context);
+void instr_ei(cpu_context_t *context);
 
 /*
-    ===========================
-        Prefix Instruction
-    ===========================
+    Prefix instructions, is called by `instr_cb_prefix`
 */
-
-/* 
-    Only acts a branching point for prefix instructions.
-*/
-void instr_exec_prefix(cpu_context_t *context);
-
-
-
-
-
-
-
-
+void instr_rlc_r8(cpu_context_t *context);
+void instr_rrc_r8(cpu_context_t *context);
+void instr_rl_r8(cpu_context_t *context);
+void instr_rr_r8(cpu_context_t *context);
+void instr_sla_r8(cpu_context_t *context);
+void instr_sra_r8(cpu_context_t *context);
+void instr_swap_r8(cpu_context_t *context);
+void instr_srl_r8(cpu_context_t *context);
+void instr_bit_b3_r8(cpu_context_t *context);
+void instr_res_b3_r8(cpu_context_t *context);
+void instr_set_b3_r8(cpu_context_t *context);
 
